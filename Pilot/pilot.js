84501c12f139e2da691863e2718f759d3c42f676
Restyled.io <commits@restyled.io>
2022-11-14 19:16:13 +0000

    Restyled by prettier

--- Pilot/pilot.js
+++ Pilot/pilot.js
@@ -1,21 +1,21 @@
-'use strict';
+"use strict";
 // const events = require( './events.js' );
 //  require('../system.js');
 //  require('../Manager/manager.js');
 
-const io = require('socket.io-client');
-const socket = io.connect('http://localhost:4000');
-const socketAirline = io.connect('http://localhost:4000/airline');
+const io = require("socket.io-client");
+const socket = io.connect("http://localhost:4000");
+const socketAirline = io.connect("http://localhost:4000/airline");
 
-socket.on('new-flight', handleNewFlight);
+socket.on("new-flight", handleNewFlight);
 
 function handleNewFlight(info) {
   setInterval(() => {
     console.log(`Pilot: flight with ID'${info.flightID}' took-off`);
-    socketAirline.emit('took-off', info);
+    socketAirline.emit("took-off", info);
   }, 4000);
   setInterval(() => {
     console.log(`Pilot: flight with ID'${info.flightID}' arrived`);
-    socket.emit('arrived', info);
+    socket.emit("arrived", info);
   }, 7000);
 }
